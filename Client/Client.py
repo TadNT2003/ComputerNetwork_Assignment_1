e10a7a84fd399f8985abc190bf0b12026cda485f
Nguyen Thanh Dat <ntdatwows2003@gmail.com>
2023-12-02 01:11:02 +0700

    Implement fetch function Add configuration guide for Windows and Linux client

--- Client/Client.py
+++ Client/Client.py
@@ -5,16 +5,24 @@ from threading import Thread
 BUFFER_SIZE = 4096
 SERVER_HOST = "192.168.31.48"
 SERVER_PORT = 5000
-CLIENT_HOST = socket.gethostname()
-CLIENT_PORT = range(15000, 15010)
-CURRENT_USE_PORT = 15000
+# Linux client will update host down the line, while Window user can get it right now
+CLIENT_HOST = socket.gethostbyname_ex(socket.gethostname())[2][2]
+CLIENT_PORT = 15000
 
-def receive_file(conn, host):
-    file_info = conn.recv(1024).decode()
+
+def receive_file(conn, fname):
+    if fname == "":
+        # Use when publish
+        filename = conn.recv(1024).decode()
+        reply = "ready for transferring"
+    else:
+        # Use when fetch
+        filename = fname
+        reply = fname
     # print(file_info)
-    filename, filesize = file_info.split("*")
+    # filename, filesize = file_info.split("*")
     # filesize = file_info[1]
-    reply = "ready for transferring"
+
     conn.send(reply.encode())
     with open(filename, "wb", 0) as received_file:
         while True:
@@ -29,18 +37,22 @@ def receive_file(conn, host):
         received_file.flush()
         os.fsync(received_file.fileno())
 
+
 def publish(lname: str, fname: str):
     # Initialize connection to server
-    global published_file
+    global published_file, CLIENT_HOST
     server_connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_connect.connect((SERVER_HOST, SERVER_PORT))
-    full_filename = lname + "\\" + fname
-    print(full_filename)
+    # Update true IP use for connection of Linux client
+    CLIENT_HOST = server_connect.getsockname()[0]
+    # Linux client use "/", Window client use "\\"
+    full_filename = lname + "/" + fname
+    # print(full_filename)
     # Catch error with open file
     try:
         published_file = open(full_filename, "rb")
     except IOError:
-            print("File not exist")
+        print("File not exist")
     else:
         # Send command request to server
         request = "publish*" + lname + "*" + fname
@@ -51,13 +63,13 @@ def publish(lname: str, fname: str):
             if check_local:
                 break
         # print(check_local)
-        if check_local == "Continue":
+        if check_local == "Continue" and lname != "":  # Only when server don't have file info AND it's not actually in local repo
             # Add file from system file to local repo
             # Connect to itself to transfer file from file system to local repo
-            self_connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self_connect.connect((CLIENT_HOST, CURRENT_USE_PORT))
-            filesize = os.path.getsize(full_filename)
-            self_connect.send(f"{fname}*{filesize}".encode())
+            self_connect = socket.socket()
+            self_connect.connect((CLIENT_HOST, CLIENT_PORT))
+            # filesize = os.path.getsize(full_filename)
+            self_connect.send(f"{fname}".encode())
             reply = self_connect.recv(1024).decode()
             # Send file
             if reply == "ready for transferring":
@@ -72,28 +84,83 @@ def publish(lname: str, fname: str):
             # Close all connections
             self_connect.close()
         else:
-            print(check_local)
-    finally:
+            print("File already in local repo")
+        # Close file
         published_file.close()
+    finally:
+        # Close socket
         server_connect.close()
+
+
+def fetch(fname: str, scrap):
+    # Initialize connection to server
+    server_connect = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_connect.connect((SERVER_HOST, SERVER_PORT))
+    request = "fetch" + "*" + fname
+    server_connect.send(request.encode())
+    # Waiting for server to check file in local repo
+    while True:
+        check_local = server_connect.recv(1024).decode()
+        if check_local:
+            break
+    if check_local == "File already in local repo":
+        print(check_local)
+    elif check_local == "File not recognize":
+        print(check_local)
+    else:
+        target_client = check_local
+        # Socket use for receive file from other client
+        target_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        target_socket.connect((target_client, CLIENT_PORT))
+        # file_receive = Thread(target=receive_file, args=(target_socket, fname))
+        # file_receive.start()
+        receive_file(target_socket, fname)
+        # Close socket to target client
+        target_socket.close()
+        # Publish the fetched file to server
+        extended_publish = Thread(target=publish, args=("", fname))
+        extended_publish.start()
+    # Close socket to server
+    server_connect.close()
+
 
 def client_listening(host, port):
     listening_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Linux client bind with "" host so don't need own host, Window client bind with its own host IP
     listening_socket.bind((host, port))
     listening_socket.listen(10)
-    connection_accepted = False
     while True:
         client_conn, client_host = listening_socket.accept()
         # print(f"Accept connection from {client_host}")
-        file_receive = Thread(target=receive_file, args=(client_conn, client_host))
-        file_receive.start()
+        # print(CLIENT_HOST)
+        if str(client_host[0]) == str(CLIENT_HOST):  # Self connect from publish command
+            file_receive = Thread(target=receive_file, args=(client_conn, ""))
+            file_receive.start()
+        else:  # Connect from other clients
+            # Receive fetch file from requested client
+            reply = client_conn.recv(1024).decode()
+            # Send file
+            print(reply)
+            with open(reply, "rb") as fetched_file:
+                while True:
+                    # Read file with a buffer size
+                    bytes_read = fetched_file.read(BUFFER_SIZE)
+                    # If end of file
+                    if not bytes_read:
+                        break
+                    # Sent to other client
+                    client_conn.send(bytes_read)
+
 
 if __name__ == "__main__":
-    for i in CLIENT_PORT:
-        main_socket = Thread(target=client_listening, args=(CLIENT_HOST, i))
-        main_socket.start()
+    listening_thread = Thread(target=client_listening, args=(CLIENT_HOST, CLIENT_PORT))
+    listening_thread.start()
 
     file_path = r"C:\Users\Admin\OneDrive\Pictures\Elden Ring"
     file_name = "First ending - Age of Stars.png"
     test_publish = Thread(target=publish, args=(file_path, file_name))
     test_publish.start()
+
+    # file_name = "First ending - Age of Stars.png"
+    # test_fetch = Thread(target=fetch, args=(file_name, 1))
+    # test_fetch.start()
--- Server/Server.py
+++ Server/Server.py
@@ -11,7 +11,7 @@ SERVER_DATABASE = {}
 # Handle client request process
 def request_listen(conn: socket.socket, host):
     request = conn.recv(1024).decode().split("*")
-    print(request)
+    # Publish command
     if request[0] == "publish":
         file_name = request[2]
         # Check file in client local repo
@@ -19,11 +19,27 @@ def request_listen(conn: socket.socket, host):
             conn.send("File already in local repo".encode())
         else:
             conn.send("Continue".encode())
-        # Add file name to server database
-        SERVER_DATABASE[host].append(file_name)
-        print(SERVER_DATABASE)
+            # Add file name to server database
+            SERVER_DATABASE[host].append(file_name)
+            print(SERVER_DATABASE)
+    # Fetch command
     elif request[0] == "fetch":
-        filename = request[1]
+        file_name = request[1]
+        # Check file in client local repo
+        if file_name in SERVER_DATABASE[host]:
+            conn.send("File already in local repo".encode())
+        else:
+            # conn.send("Continue".encode())
+            # Find the client with the file
+            found = False
+            for i in SERVER_DATABASE:
+                if file_name in SERVER_DATABASE[i]:
+                    conn.send(str(i).encode())
+                    found = True
+                    break
+            if not found:
+                # If cannot find file in DB
+                conn.send("File not recognize".encode())
     elif request[0] == "delete":
         filename = request[1]
     elif request[0] == "discover":
@@ -41,12 +57,11 @@ def client_listening(host, port):
         # If client have never connected to server before
         if not client_host[0] in SERVER_DATABASE:
             SERVER_DATABASE[client_host[0]] = []
-        print(SERVER_DATABASE)
-        file_received = Thread(target=request_listen, args=(client_conn, client_host[0]))
-        file_received.start()
+        request_handle = Thread(target=request_listen, args=(client_conn, client_host[0]))
+        request_handle.start()
 
 
 if __name__ == "__main__":
-    print(SERVER_FULLHOST)
+    # print(SERVER_FULLHOST)
     Main_Socket = Thread(target=client_listening, args=(SERVER_HOST, SERVER_PORT))
     Main_Socket.start()
